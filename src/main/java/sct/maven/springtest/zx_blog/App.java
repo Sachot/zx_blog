3d7ad9d7d9bf2d295d93aa4b80e9efab48a1749d
Sachot <caizhixuan2008@21cn.com>
2019-04-05 11:55:30 +0800

    first_commit

---
+++ src/main/java/sct/maven/springtest/zx_blog/App.java
@@ -0,0 +1,13 @@
+package sct.maven.springtest.zx_blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
